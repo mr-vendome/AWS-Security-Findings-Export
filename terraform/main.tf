dadcd9956ed9f6fe6a791d8ae6f0b29ed3677106
Marcel_VendomeDigital <marcel@vendomeinnovations.com>
2026-06-09 17:30:41 +0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -141,7 +141,7 @@ resource "aws_lambda_function" "exporter" {
   role             = aws_iam_role.lambda_execution.arn
   handler          = "lambda_function.lambda_handler"
   
-  runtime          = "python3.14"
+  runtime          = "python3.13"
   architectures    = ["arm64"]
   
   timeout          = 300
